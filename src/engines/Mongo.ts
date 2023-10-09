a379f902d54bddfc12a4152e3656a4f0eb93a3a1
MarioLemus <theliarnorland@gmail.com>
2023-10-09 16:39:08 -0600

    add engine template

---
+++ src/engines/Mongo.ts
@@ -0,0 +1,20 @@
+import { IDBMethods } from "../interfaces/IDBMethods"
+
+class Mongo implements IDBMethods {
+    getOne(): void {
+        throw new Error("Method not implemented.");
+    }
+    getAll(): void {
+        throw new Error("Method not implemented.");
+    }
+    addOne(): void {
+        throw new Error("Method not implemented.");
+    }
+    updateOne(): void {
+        throw new Error("Method not implemented.");
+    }
+    deleteOne(): void {
+        throw new Error("Method not implemented.");
+    }
+    
+}
